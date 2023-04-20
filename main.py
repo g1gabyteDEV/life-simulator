781e938baf5b6703a1b131e3ba79df9bd53f86fe
g1gabyte ⠕ <72310196+g1gabyteDEV@users.noreply.github.com>
2023-04-20 13:15:15 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-class Job:
-    def __init__(self, time: int, ):
 
+class Job:
+    def __init__(self, time: int, money_gained: int, old_money: int):
+        self.time = time
+        self.old_money = old_money
+        self.money_gained = money_gained
+        self.average = (time / money_gained) * 100
+    def work(self):
+        for second in range(self.time):
+            self.old_money = self.old_money + self.average
+            basic.pause(100)
+
+class OfficeJob(Job):
+    def __init__(self, time: int, money_gained: int, old_money: int):
+        self.__init__ = Job(time, (money_gained * 500), old_money)
+
+class Purchase:
+    def __init__(self, money_lost: int, old_money: int):
+        self.old_money = old_money
+        self.money_lost = money_lost
+    def buy(self):
+        if self.old_money > self.money_lost:
+            self.old_money = self.old_money - self.money_lost
 money = 0
+
--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
+class Job {
+    time: number
+    old_money
+    money_gained: number
+    average: number
+    constructor(time: number, money_gained: number, old_money: any) {
+        this.time = time
+        this.old_money = old_money
+        this.money_gained = money_gained
+        this.average = time / money_gained * 100
+    }
+    
+    public work() {
+        for (let second = 0; second < this.time; second++) {
+            this.old_money = this.old_money + this.average
+            basic.pause(100)
+        }
+    }
+    
+}
+
+class OfficeJob extends Job {
+}
+
+class Purchase {
+    old_money: number
+    money_lost: number
+    constructor(money_lost: number, old_money: number) {
+        this.old_money = old_money
+        this.money_lost = money_lost
+    }
+    
+    public buy() {
+        if (this.old_money > this.money_lost) {
+            this.old_money = this.old_money - this.money_lost
+        }
+        
+    }
+    
+}
+
 let money = 0
